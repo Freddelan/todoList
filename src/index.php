9ae3c405242b111295f58adb94c72036b3a7ce81
Freddelan <happyrush@hotmail.com>
2023-01-01 23:12:39 +0100

    01.01.2023

--- src/index.php
+++ src/index.php
@@ -6,16 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <title>Liste de Tâches</title>
-    <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Water+Brush&display=swap" rel="stylesheet">
+    <link href="style.css" rel="stylesheet" type="text/css" media="screen" /> 
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> -->
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Water+Brush&display=swap" rel="stylesheet"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"
         integrity="sha256-46r060N2LrChLLb5zowXQ72/iKKNiw/lAmygmHExk/o=" crossorigin="anonymous" />
 </head>
 
 <body>
+ <div class="container">
+    <div class="image">
 
+    <!-- <img src="image/liste.jpg" alt="image tasse a café" width="100%" height="auto"> </div> -->
     <header>
 
 
@@ -38,7 +41,9 @@
     <div class="todo-container">
         <ul class="todo-list"></ul>
     </div>
+</div> 
     <script src="app.js"></script>
+
 </body>
 
 </html>
